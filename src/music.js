7390f6c2944c81e23aa9be9b1f71097dbdce9def
Yakoob <yakkobkhedrii@gmail.com>
2022-12-17 00:56:54 +0330

    first commit

---
+++ src/music.js
@@ -0,0 +1,21 @@
+export default [
+
+    {
+        source: './music/Parvaz Homay - Kadkhoda (320).mp3',
+        photo: './music/Parvaz-Homay-Kadkhoda-Music-fa.com_.jpg',
+        title: 'Kadkhoda',
+        singer: 'Parvaz-Homay'
+    },
+    {
+        source: './music/Morteza Mohammadzade - Hale Delam Kharabe (320).mp3',
+        photo: './music/Morteza-Mohammadzade-Hale-Delam-Kharabe-Music-fa.com_.jpg',
+        title: 'Hale-Delam-Kharabe',
+        singer: 'Morteza-Mohammadzade'
+    },
+    {
+        source: './music/Amin Bani - Nafas Bekesh (320).mp3',
+        photo: './music/Amin-Bani-Nafas-Bekesh-Ke-Man-Az-In-Music-fa.com_.jpg',
+        title: 'Nafas-Bekesh',
+        singer: 'Amin-Bani'
+    }
+]
